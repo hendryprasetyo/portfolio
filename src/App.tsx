fc900aecc1041b41b4ae05279a0b58523ea6b64d
hendryprasetyo <hendryprras@gmail.com>
2023-09-18 08:48:38 +0700

    fix : router base in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Home from './pages/Home'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <MainLayout>
-        <Home />
-      </MainLayout>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
