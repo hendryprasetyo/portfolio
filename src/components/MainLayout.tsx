831a3a73bfedc6c37f79abda0aad50e74584225b
hendryprasetyo <hendryprras@gmail.com>
2023-09-18 08:54:01 +0700

    fix : mainlayout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,13 +1,13 @@
 import { Footer, Hero, Navbar } from '.'
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+import { Outlet } from 'react-router-dom'
+const MainLayout = () => {
   return (
     <div className="relative z-0 bg-primary overflow-hidden">
       <div className="bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
       </div>
-
-      {children}
+      <Outlet />
       <Footer />
     </div>
   )
