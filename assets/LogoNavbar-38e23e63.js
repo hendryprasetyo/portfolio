975d011b6710c4de18e4e3aebfead4364d35e1a6
hendryprasetyo <hendryprasetyo@users.noreply.github.com>
2025-07-02 02:44:49 +0000

    deploy: 6caaaa6529f31bbcbd32dac9ddb73aebad3ce49b

--- assets/LogoNavbar-38e23e63.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-1349a795.js";const t="/portfolio/original.jpg",e=()=>o.jsx("img",{src:t,alt:"logo",width:36,height:36,loading:"eager",title:"Hendry Picture",className:"w-9 h-9 rounded-full"});export{e as default};
